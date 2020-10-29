a6744c692db3f952b89e685c47c6fd7475fce25c
Ibet Hernandez <ibethernandez92@gmail.com>
2020-10-29 18:49:44 -0500

    updates app.js

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -9,8 +9,8 @@ let margin = {
         left: 60,
 };
 // Set width and height for chart 
-let chartWidth = svgWidth - margin.left- margin.right;
-let chartHeight = svgHeight - margin.top - margin.bottom;
+let width = svgWidth - margin.left- margin.right;
+let height = svgHeight - margin.top - margin.bottom;
 // Create svg wrapper
 let svg = d3
     .select(`#scatter`)
@@ -21,4 +21,29 @@ let svg = d3
 let chartGroup = svg
     .append("g")
     .attr("transfrom", `translate{$margin.left}, ${margin.top})`);
-
+    d3.csv("./assets/data/data.csv").then(function(censusData){
+        console.log(censusData);
+    
+        censusData.forEach(function(data){
+            data.poverty = +data.poverty;
+            data.healthcare = +data.healthcare;
+        });
+    let xLinearScale = d3.scaleLinear()
+        .domain([d3.min(censusData, d => d.poverty),
+        d3.max(censusData, d => d.poverty)])
+        .range([0, width]);
+    let yLinearScale = d3.scaleLinear()
+        .domain([0, d3.max(censusData, d => d.healthcare)])
+        .range([height, 0]);
+    let bottomAxis = d3.axisBottom(xLinearScale);
+    let leftAxis = d3.axisLeft(yLinearScale);
+    
+    let drawLine = d3.line()
+            .x(selectData => xLinearScale(selectData.poverty))
+            .y(selectData => yLinearScale(selectData.healthcare));
+    
+    chartGroup.append("g")
+        .classed("axis", true)
+        .call(leftAxis);
+    });
+function renderAxes()
